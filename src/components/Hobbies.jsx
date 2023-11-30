8c0a855bc6c3e77f3d94439712e492941d745bfe
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-11-30 16:19:11 -0500

    working-on-languages

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,11 +1,68 @@
-
-
 export default function Hobbies() {
-
-    return(
-        <div className="hobbiesMain">
-            <p className="hobbiesTitle">Hobbies</p>
-            <p className="hobbiesText">I enjoy playing video games, watching movies, and reading books. I also enjoy playing the guitar, and I am currently learning how to play the piano.</p>
+  return (
+    <div className="hobbiesMain">
+      <div className="hobbyImgs">
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/guitar.gif"
+            alt="guitar"
+          />
+          <p className="hobbyName"> Playing Guitar</p>
         </div>
-    )
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/cooking1.gif"
+            alt="cooking"
+          />
+          <p className="hobbyName">Cooking</p>
+        </div>
+        <div className="hobbyCont">
+          <img className="hobbyImg" src="/hobby-images/dog.gif" alt="dogs" />
+          <p className="hobbyName">My Dogs</p>
+        </div>
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/fishing.gif"
+            alt="fishing"
+          />
+          <p className="hobbyName">Fishing</p>
+        </div>
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/beer.gif"
+            alt="brewries"
+          />
+          <p className="hobbyName">Brewries</p>
+        </div>
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/gaming1.gif"
+            alt="gaming"
+          />
+          <p className="hobbyName">Gaming</p>
+        </div>
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/hiking1.gif"
+            alt="hiking"
+          />
+          <p className="hobbyName">Hiking</p>
+        </div>
+        <div className="hobbyCont">
+          <img
+            className="hobbyImg"
+            src="/hobby-images/travel2.gif"
+            alt="travel"
+          />
+          <p className="hobbyName">Traveling</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,9 +4,21 @@ import Experience from "./Experience";
 import Hobbies from "./Hobbies";
 import Languages from "./Languages";
 import SoftSkills from "./SoftSkills";
+import soundFile from "/sounds/spanishGuitar.mp3";
+
 
 export default function Resume() {
   const [selectedSection, setSelectedSection] = useState('education');
+
+  const sound = new Audio(soundFile);
+
+
+
+  const handleLanguagesClick = () => {
+    setSelectedSection("languages");
+    sound.play();
+  };
+
 
   return (
     <div className="resumeMain">
@@ -30,11 +42,11 @@ export default function Resume() {
           Hobbies
         </button>
         <button 
-          className={`resumeButton ${selectedSection === "languages" ? "resumeButtonActive" : ""}`} 
-          onClick={() => setSelectedSection("languages")}
-        >
-          Languages
-        </button>
+        className={`resumeButton ${selectedSection === "languages" ? "resumeButtonActive" : ""}`} 
+        onClick={handleLanguagesClick}
+      >
+        Languages
+      </button>
         <button 
           className={`resumeButton ${selectedSection === "soft-skills" ? "resumeButtonActive" : ""}`} 
           onClick={() => setSelectedSection("soft-skills")}
