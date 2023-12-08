8a18897fc75fb8c2f4e122ead173b99047711fe3
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-12-08 15:05:30 -0500

    img-update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="aboutMain">
     <div className="picAndPara">    
-    <img className="myPicture" src="./public/my-pic/me.JPEG" alt="" />
+    <img className="myPicture" src="./my-pic/me.JPEG" alt="" />
          <p className="aboutText"> I have been very successful with my career in the culinary industry and I believe my resume serves as a testament to my
 hard work and dedication into anything I pursue. Even when losing my job from the pandemic I was able to problem
 solve and make decisions that would turn out very successful. I am a natural leader when I am needed to be as well as a
