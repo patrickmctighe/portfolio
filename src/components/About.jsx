2f8a0c8d1414747ba99c670653aa2c127f7a3829
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-12-08 14:55:21 -0500

    updated-about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,14 @@ import{Link} from "react-router-dom";
 export default function About() {
   return (
     <div className="aboutMain">
-    
-          <p className="aboutText"> I have been very successful with my career in the culinary industry and I believe my resume serves as a testament to my
-hard work and dedication into anything a pursue. Even when losing my job from the pandemic i was able to problem
+    <div className="picAndPara">    
+    <img className="myPicture" src="./public/my-pic/me.JPEG" alt="" />
+         <p className="aboutText"> I have been very successful with my career in the culinary industry and I believe my resume serves as a testament to my
+hard work and dedication into anything I pursue. Even when losing my job from the pandemic I was able to problem
 solve and make decisions that would turn out very successful. I am a natural leader when I am needed to be as well as a
 dedicated student.
-     </p>
+     </p></div>
+ 
        <div className="letterRecCont">
 <Link className="letterRec" to="letter"> Letter of Recomendation</Link>
        </div>
