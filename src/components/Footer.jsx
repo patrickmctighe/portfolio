d30f56071f4e4c996faf94870f59bf1f561b5467
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-12-12 17:45:22 -0500

    almost-done-with-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,21 +11,31 @@ export default function Footer() {
       <div className={`circle ${activeTheme}`}></div>
       <div className={`circle ${activeTheme}`}></div>
       <div className={`circle ${activeTheme}`}></div>
+      <div className={`circle ${activeTheme}`}></div>
+      <div className={`circle ${activeTheme}`}></div>
+      <div className={`circle ${activeTheme}`}></div>
       <div className="footerGit">
         <NavLink to="https://github.com/patrickmctighe/restaurant" target="_blank">
           <img className="gitHub" src="./footer-images/githubfoot.png" alt="GitHub" />
         </NavLink>
       </div>
+      <div className={`circle ${activeTheme}`}></div>
+
       <div className="footerLinkedIn">
         <NavLink to="https://github.com/patrickmctighe/restaurant" target="_blank">
           <img className="linkedIn" src="./footer-images/linked3.png" alt="LinkedIn" />
         </NavLink>
       </div>
+      <div className={`circle ${activeTheme}`}></div>
+
       <div className="footerEmail">
         <NavLink to="https://github.com/patrickmctighe/restaurant" target="_blank">
           <img className="eMail" src="./footer-images/email2.png" alt="Email" />
         </NavLink>
       </div>
+      <div className={`circle ${activeTheme}`}></div>
+      <div className={`circle ${activeTheme}`}></div>
+
       <div className={`circle ${activeTheme}`}></div>
     </div>
   );
